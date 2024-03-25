9e9342c2b2197333976c63b33ba5e8e8e3067207
UtsavBajracharya <utsavbajra619@gmail.com>
2024-03-25 11:52:58 -0400

    modify the Flask App

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# Modify the Flask App ('app.py'):
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
@@ -5,12 +6,26 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        user_data = {
-            'name': request.form['name'],
-            'email': request.form['email']
-        }
-        return render_template('index.html', user=user_data)
+        value1 = int(request.form['value1'])
+        value2 = int(request.form['value2'])
+        operator = request.form['operator']
+        result = calculate(value1, value2, operator)
+        return render_template('index.html', result=result)
     return render_template('index.html')
+
+def calculate(value1, value2, operator):
+        if operator == '+':
+            return value1 + value2
+        elif operator == '-':
+            return value1 - value2
+        elif operator == '*':
+            return value1 * value2 
+        elif operator == '/':
+            if value2 != 0:
+                return value1 / value2
+        else:
+            return "Error: Division by zero"
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
